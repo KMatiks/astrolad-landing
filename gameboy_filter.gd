8276ffd9591a291ebbbf2d5cd0a702ec68dfdf7f
archusr <archusr@archhost.localdomain>
2023-09-16 18:16:08 -0400

    Add pass to avoid error on compilation

--- gameboy_filter.gd
+++ gameboy_filter.gd
@@ -3,7 +3,7 @@ extends ColorRect
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
