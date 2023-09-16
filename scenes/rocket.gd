5f4067000dece6dc0af34c3269646805fbb15db4
archusr <archusr@archhost.localdomain>
2023-09-16 19:20:25 -0400

    Add basic rocket physics and controls

--- scenes/rocket.gd
+++ scenes/rocket.gd
@@ -1,5 +1,8 @@
 extends RigidBody2D
 
+const THRUST = 1;
+const GRAVITY = 0.035;
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,4 +11,16 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	gravity_scale = 0.005
+	var move_speed;
+	gravity_scale = GRAVITY;
+
+	if Input.is_action_pressed("ui_right"):
+		apply_torque_impulse(THRUST)
+	if Input.is_action_pressed("ui_left"):
+		apply_torque_impulse(-THRUST)
+	if Input.is_action_pressed("ui_down"):
+		move_speed = Vector2(0, THRUST).rotated(rotation);
+		apply_central_impulse(move_speed)
+	if Input.is_action_pressed("ui_up"):
+		move_speed = Vector2(0, -THRUST).rotated(rotation);
+		apply_central_impulse(move_speed)
