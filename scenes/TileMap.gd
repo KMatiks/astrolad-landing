e0652e5d662d283fa8ac946ae729e6f40213047e
archusr <archusr@archhost.localdomain>
2023-09-16 17:23:56 -0400

    Fix camera bug. Fix viewport bug

--- scenes/TileMap.gd
+++ scenes/TileMap.gd
@@ -1,7 +1,5 @@
 extends TileMap
 
-@onready
-var viewport = get_viewport()
 
 func draw_tile(pos):
 	var atlas = Vector2(randi_range(0,3), randi_range(0,3))
@@ -10,20 +8,25 @@ func draw_tile(pos):
 
 func gen_terrain():
 	randomize()
+	var viewport = get_viewport()
 	var noise = FastNoiseLite.new()
 	var max_num_stacked_tiles = (viewport.size.y / tile_set.tile_size.y)
 
 	noise.seed = randi()
 
-	for x in range(0, viewport.size.x / tile_set.tile_size.x):
+	for x in range(0, viewport.size.x / tile_set.tile_size.x - 1):
 		var tiles_to_draw = max(ceil(abs(noise.get_noise_1d(x)) * max_num_stacked_tiles), 1)
-		for y in range(0, tiles_to_draw):
+		for y in range(0, tiles_to_draw-1):
 			draw_tile(Vector2(x, max_num_stacked_tiles - y - 1))
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	gen_terrain()
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _on_main_ready_done():
+	print(get_viewport().size)
+	gen_terrain()
--- scenes/main.gd
+++ scenes/main.gd
@@ -2,11 +2,14 @@ extends Node2D
 
 var pixel_size: int = 4;
 
+signal ready_done;
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var viewport = get_viewport();
 	var size = viewport.size;
 	viewport.size = Vector2i(size.x*pixel_size, size.y*pixel_size);
+	emit_signal("ready_done");
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
