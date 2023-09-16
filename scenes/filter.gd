48ff6fc65b05def633a170fdfbd9c0fc52207cde
archusr <archusr@archhost.localdomain>
2023-09-16 18:47:21 -0400

    Automatically size the ColorRect. Remove unused script

--- scenes/filter.gd
+++ scenes/filter.gd
@@ -9,3 +9,13 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_main_ready_done():
+	var viewport = get_viewport();
+	var children = get_children();
+
+	# should probably have typecheck here but whatever ¯\_(ツ)_/¯
+	for child in children:
+		child.size.x = viewport.size.x;
+		child.size.y = viewport.size.y;
