372fb55dec71b15e20e0144441a8ee841801b599
archusr <archusr@archhost.localdomain>
2023-09-16 23:39:31 -0400

    Refactor physics logic. Add initial trajectory logic

--- scenes/rocket.gd
+++ scenes/rocket.gd
@@ -1,6 +1,7 @@
 extends RigidBody2D
 
-const THRUST = 1;
+const THRUST = 100;
+const TORQUE_THRUST = 20;
 const GRAVITY = 0.045;
 
 var prev_frame_vel;
@@ -10,28 +11,28 @@ var prev_frame_vel;
 func _ready():
 	contact_monitor = true;
 	max_contacts_reported = 100;
+	gravity_scale = GRAVITY;
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var move_speed;
-	gravity_scale = GRAVITY;
 
 	if Input.is_action_pressed("ui_right"):
-		apply_torque_impulse(THRUST)
+		apply_torque(TORQUE_THRUST)
 	if Input.is_action_pressed("ui_left"):
-		apply_torque_impulse(-THRUST)
+		apply_torque(-TORQUE_THRUST)
 	if Input.is_action_pressed("ui_down"):
 		move_speed = Vector2(0, THRUST).rotated(rotation);
-		apply_central_impulse(move_speed)
+		apply_central_force(move_speed)
 	if Input.is_action_pressed("ui_up"):
 		move_speed = Vector2(0, -THRUST).rotated(rotation);
-		apply_central_impulse(move_speed)
+		apply_central_force(move_speed)
 
 	prev_frame_vel = linear_velocity;
 
 
 func _on_body_entered(body):
-	print(prev_frame_vel)
+	#print(prev_frame_vel)
 	if body is TileMap and (abs(prev_frame_vel.y) > 10 or abs(prev_frame_vel.x) > 10):
 		print("Crashed!")
---
+++ scenes/trajectory.gd
@@ -0,0 +1,35 @@
+extends Line2D
+
+@onready
+var rocket = get_parent();
+var prev_vel = Vector2.ZERO;
+var cur_vel = Vector2.ZERO;
+const MAX_POINTS = 300
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+func update_trajectory(delta: float) -> void:
+	clear_points();
+	var gravity = rocket.gravity_scale;
+	cur_vel = rocket.get_linear_velocity() * rocket.global_transform.x;
+	var acceleration = Vector2((cur_vel.x - prev_vel.x) / delta, (cur_vel.y - prev_vel.y) / delta);
+	var pos = rocket.global_position
+	add_point(pos)
+	print(rocket.global_position)
+	for pt in range(MAX_POINTS):
+		add_point(pos);
+		cur_vel.y += gravity;
+		pos += cur_vel * delta;
+
+#		if pos.y > rocket.position.y - 25:
+#			break
+
+	prev_vel = cur_vel;
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var heading_in_degrees = rad_to_deg(rocket.rotation);
+	update_trajectory(delta);
+	show()
